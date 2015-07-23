153df4d4f3fa06665b007a8ae9d40cb3fcd92656
Kevin Deisz <kevin.deisz@gmail.com>
2015-07-23 18:02:18 -0400

    add lexer

---
+++ lib/dwek/lexer.rb
@@ -0,0 +1,38 @@
+module Dwek
+  class Lexer
+
+    def initialize(verbose = false)
+      @verbose = verbose
+      @tokens = []
+    end
+
+    def parse(string)
+      until string.empty?
+        skipped = false
+        case string
+        when /\A\/\*.*?\*\//m, /\A(?:\n|\s+|\/\/[^\n]+)/
+          skipped = true
+        when /\A\{(\w+)\}/
+          @tokens << [:MAPPER, $1]
+        when /\A(?:map|as|with|and|=|\[|\]|\,|;)/i
+          @tokens << [$&.upcase, nil]
+        when /\A@(\w+)/
+          @tokens << [:VARIABLE, $1]
+        when /\A(\w+)/
+          @tokens << [:OPTION, $1]
+        when /\A\'(\w+)\'/, /\A\"(\w+)\"/
+          @tokens << [:STRING, $1]
+        else
+          raise SyntaxError, "can't parse #{string.first(10)}"
+        end
+        puts @tokens.last.inspect unless skipped || !@verbose
+        string = $'
+      end
+      @tokens << [false, '$end']
+    end
+
+    def next_token
+      @tokens.shift
+    end
+  end
+end
