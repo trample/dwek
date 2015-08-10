ef65dd085f35fdb39847c2c5255ee3bc15ed2dfc
Kevin Deisz <kevin.deisz@gmail.com>
2015-08-10 14:47:04 -0400

    updating test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_record'
 require 'minitest/autorun'
 require 'dwek'
-require 'fixtures'
+require 'support/fixtures'
 
 ActiveSupport.test_order = :sorted
